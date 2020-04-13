82695cc7a22c9e718869fdc3cebeaba548855150
Togg-streamdeck <bruno_boyer@yahoo.fr>
2020-04-13 14:57:48 +0200

    update

---
+++ a10c/Keyboard.diff.lua
@@ -0,0 +1,357 @@
+local diff = {
+	["keyDiffs"] = {
+		["d1304pnilu1304cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "W",
+				},
+			},
+			["name"] = "UFC 0 Key",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num0",
+					["reformers"] = {
+						[1] = "LCtrl",
+						[2] = "LShift",
+					},
+				},
+			},
+		},
+		["d1305pnilu1305cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "S",
+					["reformers"] = {
+						[1] = "LCtrl",
+						[2] = "LShift",
+					},
+				},
+			},
+			["name"] = "UFC SPC Key",
+		},
+		["d1306pnilu1306cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "H",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "UFC HACK Key",
+		},
+		["d1307pnilu1307cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "F",
+					["reformers"] = {
+						[1] = "LCtrl",
+						[2] = "LShift",
+					},
+				},
+			},
+			["name"] = "UFC FUNC Key",
+		},
+		["d1308pnilu1308cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "L",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "UFC LTR Key",
+		},
+		["d1309pnilu1309cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "U",
+					["reformers"] = {
+						[1] = "LCtrl",
+						[2] = "LShift",
+					},
+				},
+			},
+			["name"] = "UFC CLR Key",
+		},
+		["d1311pnilu1311cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "K",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "UFC MK Key",
+		},
+		["d1312pnilu1312cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Y",
+					["reformers"] = {
+						[1] = "LCtrl",
+						[2] = "LShift",
+					},
+				},
+			},
+			["name"] = "UFC ALT Key",
+		},
+		["d1374pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "M",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "Master switch ARM",
+		},
+		["d1375pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "M",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Master switch SAFE",
+		},
+		["d1376pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "M",
+					["reformers"] = {
+						[1] = "RCtrl",
+						[2] = "RShift",
+					},
+				},
+			},
+			["name"] = "Master switch TRAIN",
+		},
+		["d1377pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "A",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "GUN/PAC switch ARM",
+		},
+		["d1378pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "A",
+					["reformers"] = {
+						[1] = "RCtrl",
+						[2] = "RShift",
+					},
+				},
+			},
+			["name"] = "GUN/PAC switch SAFE",
+		},
+		["d1379pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "A",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "GUN/PAC switch GUNARM",
+		},
+		["d1383pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "T",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "TGP switch ON/OFF",
+		},
+		["d1384pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "B",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "ALT SCE switch BARO",
+		},
+		["d1385pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "D",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "ALT SCE switch DELTA",
+		},
+		["d1386pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "R",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "ALT SCE switch RADAR",
+		},
+		["d1389pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "C",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "CICU switch ON/OFF",
+		},
+		["d1390pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "J",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "JTRS switch ON/OFF",
+		},
+		["d1399pnilu1399cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "U",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "NMSP HARS button",
+		},
+		["d1400pnilu1400cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "T",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "NMSP EGI button",
+		},
+		["d1401pnilu1401cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "D",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "NMSP TISL button",
+		},
+		["d1402pnilu1402cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "O",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "NMSP STR PT button",
+		},
+		["d1403pnilu1403cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "R",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "NMSP ANCHR button",
+		},
+		["d1404pnilu1404cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "S",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "NMSP TCN button",
+		},
+		["d1405pnilu1405cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "I",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "NMSP ILS button",
+		},
+		["d1406pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "E",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "NMSP ABLE/STOW switch",
+		},
+		["d387pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "LAAP Altitude/Bank Hold",
+			["removed"] = {
+				[1] = {
+					["key"] = "4",
+				},
+			},
+		},
+		["d554pnilu638cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Space",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "Gun Trigger",
+			["removed"] = {
+				[1] = {
+					["key"] = "Space",
+				},
+			},
+		},
+		["d74pnilu75cdnilvdnilvpnilvunil"] = {
+			["name"] = "Wheel Brake On/Off",
+			["removed"] = {
+				[1] = {
+					["key"] = "W",
+				},
+			},
+		},
+	},
+}
+return diff
---
+++ f14/Keyboard.diff.lua
@@ -0,0 +1,295 @@
+local diff = {
+	["keyDiffs"] = {
+		["d3038pnilunilcd21vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "E",
+					["reformers"] = {
+						[1] = "LCtrl",
+						[2] = "LShift",
+					},
+				},
+			},
+			["name"] = "Altitude Hold Off",
+		},
+		["d3038pnilunilcd21vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "W",
+					["reformers"] = {
+						[1] = "LCtrl",
+						[2] = "LShift",
+					},
+				},
+			},
+			["name"] = "Altitude Hold On",
+		},
+		["d3040pnilunilcd21vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Q",
+					["reformers"] = {
+						[1] = "LCtrl",
+						[2] = "LShift",
+					},
+				},
+			},
+			["name"] = "Autopilot Off",
+		},
+		["d3040pnilunilcd21vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "A",
+					["reformers"] = {
+						[1] = "LCtrl",
+						[2] = "LShift",
+					},
+				},
+			},
+			["name"] = "Autopilot On",
+		},
+		["d3130pnilu3130cd55vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "I",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "Gun rate toggle",
+		},
+		["d3139pnilu3139cd55vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "O",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "AIM-9 cooling toggle",
+		},
+		["d3140pnilu3140cd55vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Y",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "Missile prepare toggle",
+		},
+		["d3141pnilu3141cd55vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "T",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "Missile mode toggle",
+		},
+		["d3566pnilunilcd62vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "'",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Nav - Set Steerpoint SP 1",
+		},
+		["d3567pnilunilcd62vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ";",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Nav - Set Steerpoint SP 2",
+		},
+		["d3568pnilunilcd62vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ",",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Nav - Set Steerpoint SP 3",
+		},
+		["d3569pnilunilcd62vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "9",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Nav - Set Steerpoint Fixed Point",
+		},
+		["d3570pnilunilcd62vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "-",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Nav - Set Steerpoint Initial Point",
+		},
+		["d3571pnilunilcd62vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "/",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Nav - Set Steerpoint Surface Target",
+		},
+		["d3572pnilunilcd62vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "0",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Nav - Set Steerpoint Home Base",
+		},
+		["d3573pnilunilcd62vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "=",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Nav - Set Steerpoint Man",
+		},
+		["d3574pnilunilcd62vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "J",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Radar - VSL High",
+		},
+		["d3575pnilunilcd62vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "K",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Radar - VSL Low",
+		},
+		["d3576pnilunilcd62vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "D",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Radar - Toggle PD-STT or P-STT",
+		},
+		["d3577pnilunilcd62vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "L",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Radar - Break Lock",
+		},
+		["d3649pnilunilcd11vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "U",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "Lights ACM Brightnes Down",
+		},
+		["d3649pnilunilcd11vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "I",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "Lights ACM Brightnes Up",
+		},
+		["d3657pnilunilcd62vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ".",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Loud and Clear",
+		},
+		["d52pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "P",
+				},
+			},
+			["name"] = "Pause",
+			["removed"] = {
+				[1] = {
+					["key"] = "Pause",
+				},
+			},
+		},
+		["d816pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "P",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "Active Pause",
+			["removed"] = {
+				[1] = {
+					["key"] = "Pause",
+					["reformers"] = {
+						[1] = "LShift",
+						[2] = "LWin",
+					},
+				},
+			},
+		},
+	},
+}
+return diff
